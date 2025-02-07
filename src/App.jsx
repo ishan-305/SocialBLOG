82fa8ec59756878496d79b24c9276260acd9dc41
Ishan <ishansingh2390@gmail.com>
2025-02-08 00:33:31 +0530

    Debug 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth_service";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
-  return (
-    <>
-      <div className="text-amber-400">Hello World</div>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-700">
+      <div className="w-full block">
+        <Header />
+        <main>TODO : {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
