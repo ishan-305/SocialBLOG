e4057752bc00d5c0eccb4c33920b53c646ad9983
Ishan <ishansingh2390@gmail.com>
2025-02-09 12:17:17 +0530

    Added More Components

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React, { Children } from "react";
+
+function Button({
+  children,
+  type = "button",
+  textColor = "text-white",
+  bgColor = "bg-blue-500",
+  classname = "",
+  ...props
+}) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg ${classname} ${textColor} ${bgColor}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
