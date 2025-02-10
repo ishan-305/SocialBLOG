a62176948513ee77a82490756f05939ecbd76e29
Ishan <ishansingh2390@gmail.com>
2025-02-10 18:11:51 +0530

    Login and SignUp added

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export { Header, Footer, Logo, Container, LogoutBtn, Input, Button };
